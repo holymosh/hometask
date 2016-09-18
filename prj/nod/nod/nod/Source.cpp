16448db4c1263991e4f395b2431c088b26de8b1e
holymosh <skyland7494@mail.ru>
2016-09-18 13:38:53 +0300

    Renamed function nod.

--- prj/nod/nod/nod/Source.cpp
+++ prj/nod/nod/nod/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+
 using namespace std;
-int Nod(int firstValue, int secondValue) {
+
+int nod(int firstValue, int secondValue) {
 	int c;
 	while (secondValue)
 	{
@@ -11,16 +13,21 @@ int Nod(int firstValue, int secondValue) {
 	}
 	return abs(firstValue);
 }
+
 int main()
 {
-	int firstValue = 8, secondValue = 16;
-	cout<<"nod(8,16) :"<<Nod(firstValue, secondValue)<<"\n";
-	firstValue = -9, secondValue = 12;
-	cout << "nod(-9,12) :" << Nod(secondValue, firstValue) << "\n";
-	firstValue = -11, secondValue = -5;
-	cout << "nod(-5,-11) :" << Nod(secondValue, firstValue) << "\n";
-	firstValue = 100, secondValue = 100;
-	cout << "nod(100,100) :" << Nod(secondValue, firstValue) << "\n";
+	int firstValue = 8;
+    	int secondValue = 16;
+	cout<<"nod(8,16) :"<<nod(firstValue, secondValue)<<"\n";
+	firstValue = -9;
+	secondValue = 12;
+	cout << "nod(-9,12) :" << nod(secondValue, firstValue) << "\n";
+	firstValue = -11;
+	secondValue = -5;
+	cout << "nod(-5,-11) :" << nod(secondValue, firstValue) << "\n";
+	firstValue = 100;
+	secondValue = 100;
+	cout << "nod(100,100) :" << nod(secondValue, firstValue) << "\n";
 	return 0;
 	
 }

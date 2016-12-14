c252fa37b433a034108f45ab95cd17b6bc6d8b62
holymosh <skyland7494@mail.ru>
2016-12-14 18:26:41 +0300

    course work - valuesBuilder.h

--- popov_d_d/prj/appheatmap/appheatmap/valuesBuilder.h
+++ popov_d_d/prj/appheatmap/appheatmap/valuesBuilder.h
@@ -10,9 +10,12 @@ public:
 	ValuesBuilder(maptypes::maptype type);
 	~ValuesBuilder();
 	ValuesBuilder(const ValuesBuilder& other);
+	Mat_<Scalar> getValuesMatrix(const Mat& values, maptypes::maptype mtype);
 	maptypes::maptype maptype{maptypes::first};
 private:
+	Mat_<Scalar> getFirstScalarMatType(const Mat& values);
 	Mat& getSecondScalarMatType(const Mat& values);
+	Scalar getScalarForFirstType(int value);
 
 
 };
